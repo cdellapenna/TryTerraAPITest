8387cd63c3794910439e08c43305256221ce9dbf
cdellapenna <cassiusdp@gmail.com>
2023-10-06 15:25:52 -0400

    terraclient

--- terraClient.py
+++ terraClient.py
@@ -1,23 +1,19 @@
-import datetime
+# Importing the API and instantiating the client using your keys
 from terra.base_client import Terra
-from terra.models.user import User
-# ... other necessary imports
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-# Initialize the Terra client
-terra = Terra(api_key="ussv5SAQ53a1nNTxsMr9G41zj2KUhYMk5eDU1hjG", dev_id="testingTerra", secret="SECRETKEYHERE")
+API_KEY = os.environ.get('API_KEY')
+DEV_ID = os.environ.get('DEV_ID')
+SECRET = os.environ.get('SIGN')
 
-# Assume you have a User object, if not, create or retrieve one
-user = User(user_id="b476fcf5-e55e-4d06-9f13-306003329e9b", provider="GOOGLE")
 
-# Define the start and end dates for data retrieval
-start_date = datetime.datetime.now() - datetime.timedelta(days=7)  # 7 days ago
-end_date = datetime.datetime.now()  # now
+terra = Terra(API_KEY, DEV_ID, SECRET)
 
-# Retrieve activity data
-response = terra.get_activity_for_user(user, start_date, end_date, to_webhook=True)
+parsed_api_response = terra.list_providers().get_parsed_response()
+print(parsed_api_response)
 
-# Handle the response
-if response.is_successful():
-    print("Data retrieval initiated successfully!")
-else:
-    print("Error:", response.error_message)
+parsed_api_response = terra.list_users().get_parsed_response()
+print(parsed_api_response)
+
